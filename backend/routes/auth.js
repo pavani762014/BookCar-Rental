56b941e497bc3956bc0826465d530a898afbe48d
Pavani Vegi <pavani762014@gmail.com>
2025-11-17 21:43:29 +0530

    fixed cors issue

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post("/login", loginUser);
+router.post("/register", registerUser);
+
 
 module.exports = router;
