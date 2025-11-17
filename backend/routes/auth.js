40b0751d6d22141bb317eed5d31f886cb70e4b2b
Pavani Vegi <pavani762014@gmail.com>
2025-11-17 22:08:43 +0530

    update all files

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,8 @@
-// backend/routes/auth.js
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-// login
 router.post("/login", authController.login);
-
-// register
 router.post("/register", authController.register);
 
 module.exports = router;
